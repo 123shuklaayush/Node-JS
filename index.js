d8b465da7ec693fa547f5efbba073544f226ca5d
Ayush Shukla <88582186+123shuklaayush@users.noreply.github.com>
2022-09-06 22:12:06 +0530

    Add files via upload
    Express JS

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
-const connections = require("./second")
-console.log("hello  world",connections)
+
+const express = require('express')
+const path = require('path')
+const app = express()
+const port = 3000
+
+app.use(express.static(path.join(__dirname, "public")))
+
+app.get('/hello', (req, res) => {
+  res.send('Hello World!')
+  // res.sendFile(path.join(__dirname, 'index.html'))
+})
+
+app.get('/contact.html', (req, res) =>{
+  // res.sendFile(path.join(__dirname, 'contact.html'))
+  // res.status(500)
+  res.json({"Harry":42})
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on http://localhost:${port}`)
+})
